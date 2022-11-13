5a5fb587859b5093612cbc5a498c992f89945895
JoonJR <joonajani.rantala@gmail.com>
2022-11-13 21:25:17 +0200

    .

--- ModuleOne/JsFive.js
+++ ModuleOne/JsFive.js
@@ -1,14 +1,13 @@
 "use strict";
-const year = parseInt(prompt("Enter a year to check leap year"))
+const year = parseInt(prompt("Enter a year to check leap year"));
 
-if(year % 4 === 0){
-  if(year % 100 === 0 && year % 400 !== 0) {
-    document.querySelector('#result').innerHTML = `The year ${year} is not a leap year`
-  }
-  else{
-    document.querySelector('#result').innerHTML = `The year ${year} is a leap year`
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 === year % 4) && (0 !== year % 100) || (0 === year % 400)) {
+        document.getElementById("result").innerHTML = year + ' is a leap year';
+    } else {
+        document.getElementById("result").innerHTML = year + ' is not a leap year';
     }
 }
-else {
-  document.querySelector('#result').innerHTML = `The year ${year} is not a leap year`
-}
+checkLeapYear(year);
