e8e0bdfe0b74b6347cd5669158d470b8b78b759e
JoonJR <joonajani.rantala@gmail.com>
2022-11-13 16:32:50 +0200

    .

--- ModuleOne/JsFour.js
+++ ModuleOne/JsFour.js
@@ -1,1 +1,26 @@
 "use strict";
+window.alert("Welcome to Hogwarts School of Witchcraft and Wizardry");
+let name = window.prompt("Enter your name");
+document.querySelector('#hello').innerHTML = 'Hello, ' + name + '! Click draw to get a room!';
+
+function drawRoom() {
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(4);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const int = getRandomIntInclusive()
+  if(int === 1){
+    document.getElementById("result").innerHTML = name + ' you are Gryffindor.';
+  }
+  else if(int === 2){
+    document.getElementById("result").innerHTML = name + ' you are Slytherin.';
+  }
+  else if(int ===3){
+    document.getElementById("result").innerHTML = name + ' you are Hufflepuff.';
+  }
+  else if(int === 4){
+    document.getElementById("result").innerHTML = name + ' you are Ravenclaw.';
+}
+}
+
